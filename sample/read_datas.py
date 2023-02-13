02e892ab01df556b193e2e377efeeb5949312cd0
夜间清风 <1874032283@qq.com>
2023-02-14 03:54:49 +0800

    重构之前保存一下，以免出现意外

--- sample/read_datas.py
+++ sample/read_datas.py
@@ -1,7 +1,6 @@
 import CSF
 import numpy as np
 import open3d as o3d
-from traitlets import Bool
 
 class ReadDatas:
 
@@ -11,9 +10,15 @@ class ReadDatas:
         self.datas = o3d.io.read_point_cloud(self.cloud)
         self.xyz = np.asarray(self.datas.points)
         self.ground = None
-        self.non_ground = None 
+        self.non_ground = None
+        self.neighbour_number = 30
+        self.curvity = []
 
-    def SVD(self,points):
+        self.theta_threshold = 15
+        self.cosine_threshold = np.cos(np.deg2rad(self.theta_threshold))
+        self.curvature_threshold=0.035
+
+    def SVD(self,points:'ndarray') -> list:
         # 二维，三维均适用
         # 二维直线，三维平面
         pts = points.copy()
@@ -34,11 +39,12 @@ class ReadDatas:
         # normal >>> 拟合的方向向量
         return u,s,c,normal
     
-    def estimate_parameters(self,pts):
+    def estimate_parameters(self,points:'ndarray') -> list:
         # 最小二乘法估算平面模型
         # 只有三个点时，可以直接计算
+        
 
-        _,_,c,n = self.SVD(pts)
+        _,_,c,n = self.SVD(points)
 
         params = np.hstack((c.reshape(1,-1),n.reshape(1,-1)))[0,:]
         return params
@@ -46,17 +52,9 @@ class ReadDatas:
 
 
 
-    # def visual_3dmap(self,cloud):
-    #     '''
-    #     Visual 3dmap
-    #     '''
-    #     clouds = ['cloud{}'.format(i)= o3d.geometry.PointCloud() for i in range(len(cloud))]
 
-    #     cloud = [o3d.utility.Vector3dVector(xyz) for xyz in cloud]
 
-    #     o3d.visualization.draw_geometries(cloud)
-
-    def csf_ground_extraction(self,bSloopSmooth :Bool = False,cloth_resolution :float = 0.5):
+    def csf_ground_extraction(self,bSloopSmooth:'Bool' = False,cloth_resolution :float = 0.5):
         
         csf = CSF.CSF()
         csf.params.bSloopSmooth = bSloopSmooth
@@ -84,7 +82,7 @@ class ReadDatas:
 
         seed_cp = seed
         for i in range(10):    
-            params = self.estimate_parameters(pts=seed_cp)
+            params = self.estimate_parameters(seed_cp)
             h = ((xyz_cp[:,0]-params[0]) * params[3] + (xyz_cp[:,1]-params[1])* params[4]+
                 (xyz_cp[:,2]-params[2])* params[5])/(np.sqrt(params[3]**2+params[4]**2+params[5]**2))
             seed_cp = xyz_cp[h < ground_h]
@@ -98,28 +96,112 @@ class ReadDatas:
                                                         ransac_n = ransac_n,num_iterations = num_iterations)
         inliers =  np.array(inliers)
         self.ground = self.xyz[inliers, :]
-        # self.non_ground = self.xyz[~inliers, :]
+        self.non_ground = np.delete(self.xyz, inliers, axis = 0)
+
+    def curvature_calculation(self,clouds:'o3d'):
+        num_of_pts = len(clouds.points)         # 点云点的个数
+        clouds.estimate_covariances(o3d.geometry.KDTreeSearchParamKNN(self.neighbour_number))
+        cov_mat = clouds.covariances            # 获取每个点的协方差矩阵
+        self.curvity = np.zeros(num_of_pts)          # 初始化存储每个点曲率的容器
+        # point_curvature_index = np.zeros((num_of_pts, 2))
+        # 计算每个点的曲率
+        for i_n in range(num_of_pts):
+            eignvalue, _ = np.linalg.eig(cov_mat[i_n])  # SVD分解求特征值
+            idx = eignvalue.argsort()[::-1]
+            eignvalue = eignvalue[idx]
+            self.curvity[i_n] = eignvalue[2] / (eignvalue[0] + eignvalue[1] + eignvalue[2])
+        
+        self.curvity = np.array(self.curvity)
+
+        return self.curvity
+    
+    def np_to_o3d(self,clouds:'ndarray'):
+        name = ''
+        name = o3d.geometry.PointCloud()
+        name.points = o3d.utility.Vector3dVector(clouds)
+        name.estimate_normals()
+
+        return name
+
+    def find_neighbour_points(self,clouds,neighbour_number = 30 ):
+        kdtree = o3d.geometry.KDTreeFlann(clouds)
+        number = len(clouds.points)
+        
+        point_neighbours = np.zeros((number, neighbour_number))
+        for ik in range(number):
+            [_, idx, _] = kdtree.search_knn_vector_3d(clouds.points[ik], neighbour_number)  # K近邻搜索
+            point_neighbours[ik, :] = idx
+        return point_neighbours 
+    
+    def region_growing_segmentation(self,seed,clouds,no_paves):
+
+        nebor_all = self.find_neighbour_points(clouds)
+        curvity = self.curvature_calculation(clouds)
+
+        paves = np.array(seed) 
+        while len(seed)>0:
+            seed_now = seed[0]
+            nebor = nebor_all[seed_now]
+            nebor = np.asarray(nebor)
+
+
+            nebor_np = nebor[np.isin(nebor,paves,invert=True)]
+            nebor_new = nebor_np[np.isin(nebor_np,seed,invert=True)]
+            nebor_new = nebor_new[np.isin(nebor_new,no_paves,invert=True)]
+
+
+            if len(nebor_new)>0:
+
+                curr_seed_normal = clouds.normals[seed_now]       # 当前种子点的法向量
+                seed_nebor_normal = [clouds.normals[int(i)]  for i in nebor_new]     # 种子点邻域点的法向量
+                dot_normal = np.fabs(np.dot(seed_nebor_normal, curr_seed_normal))
+                nebor_new = nebor_new.astype('int64')
+                curvity_now= curvity[nebor_new]
+                a = dot_normal > self.cosine_threshold
+                b = curvity_now < self.curvature_threshold
+                c = a&b
+                paves_new = nebor_new[c]
+                paves = np.append(paves,paves_new)
+                seed = np.append(seed,paves_new)
+
+            seed = np.delete(seed,[0])
+    
+        return paves
+
+ 
+                
+
+    
+
+        
+
 
 
         
         
-
-
-
-
-
-
-
-
-
-
 
 if __name__ == '__main__':
 
     datas = ReadDatas('Point Cloud Data\Corner.ply')
     print(datas.datas)
-    datas.ransac_ground_extraction()
-    print(datas.ground.shape)
+    datas.gpf_ground_extraction()
+    
+    clouds = datas.np_to_o3d(datas.ground,name = 'clouds')
+    curvity = datas.curvature_calculation(clouds)
+    print('-----------')
+    seed = np.argmin(curvity)
+    seed = np.array([seed])
+    print('-----------')
+    neighbors = datas.find_neighbour_points(clouds)
+    print('-----------')
+    paves = datas.region_growing_segmentation(seed, neighbors,curvity,clouds)
+    print('-----------')
+    print(len(paves))
+    print('-----------')
+
+
+
+    
 
 
     
